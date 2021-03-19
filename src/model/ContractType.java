3e30e68ea71201093ccf73943352ea9d80fcbf4a
Nam Anh <tnnamanh@gmail.com>
2021-03-19 17:39:58 +0700

    Add Grade config + fix DAO and Model

--- src/model/ContractType.java
+++ src/model/ContractType.java
@@ -3,12 +3,12 @@ package model;
 public class ContractType {
 
 	private int id;
-	private String name;
+	private String type;
 
-	public ContractType(int id, String name) {
+	public ContractType(int id, String type) {
 		super();
 		this.id = id;
-		this.name = name;
+		this.type = type;
 	}
 
 	public ContractType() {
@@ -23,12 +23,12 @@ public class ContractType {
 		this.id = id;
 	}
 
-	public String getName() {
-		return name;
+	public String getType() {
+		return type;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public void setType(String type) {
+		this.type = type;
 	}
 
 }
