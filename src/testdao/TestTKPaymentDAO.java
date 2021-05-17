ddfcdb08a0ad336a6156c03307098d94cb5f2797
Nam Anh <tnnamanh@gmail.com>
2021-05-17 16:28:31 +0700

    Add Junit for TKPaymentDAO

---
+++ src/testdao/TestTKPaymentDAO.java
@@ -0,0 +1,25 @@
+package testdao;
+
+import org.junit.jupiter.api.Test;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import dao.TKPaymentDAO;
+
+class TestTKPaymentDAO {
+
+//	@Test
+//	void testGetPaymentStats() {
+//		TKPaymentDAO testDAO = new TKPaymentDAO();
+//		try {
+//			ResultSet resultSet = testDAO.getPaymentStats();
+//			while(resultSet.next()) {
+//				
+//			}
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//	}
+
+}
