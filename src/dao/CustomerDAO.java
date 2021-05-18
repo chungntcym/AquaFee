df77e4da6edbb26fe3f49c93b40b52c0cf8ffd8f
Tuananhvubg <54315227+Tuananhvubg@users.noreply.github.com>
2021-05-18 21:44:43 +0700

    Update CustomerDAO

--- src/dao/CustomerDAO.java
+++ src/dao/CustomerDAO.java
@@ -8,10 +8,10 @@ import java.util.ArrayList;
 import model.Customer;
 
 public class CustomerDAO extends DAO{
+	private final Connection connection = getConnection();
 	private static final String GET_ALL_ADDRESS = "SELECT * FROM `tblcustomer`;";
 	public ArrayList<Customer> getAddress() throws SQLException {
 		try {
-			Connection connection = getConnection();
 			// try-with-resource statement will auto close the connection.
 			PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_ADDRESS);
 			ResultSet resultSet = preparedStatement.executeQuery();
@@ -20,7 +20,7 @@ public class CustomerDAO extends DAO{
 				Customer customer = new Customer();
 				customer.setId(resultSet.getInt("id"));
 				customer.setAddress(resultSet.getString("address"));
-				customer.setEmail(resultSet.getString("email"));
+				customer.setFullname(resultSet.getString("fullname"));
 				listCustomer.add(customer);
 			}
 			
@@ -33,7 +33,6 @@ public class CustomerDAO extends DAO{
 	private static final String GET_ALL_BY_ID = "SELECT * FROM `tblcustomer` Where id=?;";
 	public ArrayList<Customer> getAll(String ID) throws SQLException {
 		try {
-			Connection connection = getConnection();
 			// try-with-resource statement will auto close the connection.
 			PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_BY_ID);
 			preparedStatement.setString(1, ID);
@@ -42,9 +41,9 @@ public class CustomerDAO extends DAO{
 			while (resultSet.next()) {
 				Customer customer = new Customer();
 				customer.setId(resultSet.getInt("id"));
-				customer.setFullname(resultSet.getString("fullname"));
-				customer.setEmail(resultSet.getString("email"));
 				customer.setAddress(resultSet.getString("address"));
+				customer.setEmail(resultSet.getString("email"));
+				customer.setFullname(resultSet.getString("fullname"));
 				listCustomer.add(customer);
 			}
 			
@@ -54,29 +53,8 @@ public class CustomerDAO extends DAO{
 			return null;
 		}
 	}
-	private static final String GET_ALL_CUSTOMER_BY_ID = "SELECT * FROM `tblCustomer' WHERE tblSubDistrictid=?;";
 
-	public ArrayList<Customer> getCustomerByID(String id) throws SQLException {
-		try {
-			Connection connection = getConnection();
-			// try-with-resource statement will auto close the connection.
-			PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_CUSTOMER_BY_ID);
-			preparedStatement.setString(1, id);
-			ResultSet resultSet = preparedStatement.executeQuery();
-			ArrayList<Customer> listCustomer = new ArrayList<>();
-			while (resultSet.next()) {
-				Customer customer = new Customer();
-				customer.setId(resultSet.getInt("id"));
-				customer.setFullname(resultSet.getString("fullname"));
-				customer.setAddress(resultSet.getString("address"));
-				customer.setEmail(resultSet.getString("email"));
-				listCustomer.add(customer);
-			}
-			
-			return listCustomer;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		}
+	public Connection getDAOConnection() {
+		return this.connection;
 	}
 }
