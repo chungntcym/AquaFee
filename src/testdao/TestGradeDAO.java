209aa3e4568efccc3b3b6c8a9a7bf8ae170d4565
Nakajima-Kuro <tnnamanh@gmail.com>
2021-05-19 08:42:35 +0700

    back to 1 test data 1 junit

--- src/testdao/TestGradeDAO.java
+++ src/testdao/TestGradeDAO.java
@@ -15,14 +15,14 @@ import model.Grade;
 class TestGradeDAO extends DAO {
 
 	@Test
-	void testGetTableGradeByContractTypeId() throws Exception {
+	void testGetTableGradeByContractTypeId1() throws Exception {
 		GradeDAO gradeDao = new GradeDAO();
 
 		Connection connection = gradeDao.getDAOConnection();
 		connection.setAutoCommit(false);
 		try {
 
-			//Test 1: Normal
+			// Test 1: Normal
 			Date date = new Date();
 			java.sql.Timestamp startDate = new java.sql.Timestamp(date.getTime());
 
@@ -37,16 +37,6 @@ class TestGradeDAO extends DAO {
 			ArrayList<Grade> listGradeResult = gradeDao.getTableGradeByContractTypeId("1");
 
 			// If pass => both create and read function work normally
-			assertEquals(listGradeTest, listGradeResult);
-			
-			//Test 2: Negative id
-			listGradeResult = gradeDao.getTableGradeByContractTypeId("-1");
-			listGradeTest = new ArrayList<>();
-			assertEquals(listGradeTest, listGradeResult);
-			
-			//Test 3: Wrong type
-			listGradeResult = gradeDao.getTableGradeByContractTypeId("abcd");
-			listGradeTest = new ArrayList<>();
 			assertEquals(listGradeTest, listGradeResult);
 
 		} finally {
@@ -54,16 +44,54 @@ class TestGradeDAO extends DAO {
 			connection.close();
 		}
 	}
-	
+
 	@Test
-	void testSaveGradeTable() throws Exception {
+	void testGetTableGradeByContractTypeId2() throws Exception {
 		GradeDAO gradeDao = new GradeDAO();
 
 		Connection connection = gradeDao.getDAOConnection();
 		connection.setAutoCommit(false);
 		try {
-			
-			//Test 1: Normal
+
+			// Test 2: Negative id
+			ArrayList<Grade> listGradeResult = gradeDao.getTableGradeByContractTypeId("-1");
+			ArrayList<Grade> listGradeTest = new ArrayList<>();
+			assertEquals(listGradeTest, listGradeResult);
+
+		} finally {
+			connection.rollback();
+			connection.close();
+		}
+	}
+
+	@Test
+	void testGetTableGradeByContractTypeId3() throws Exception {
+		GradeDAO gradeDao = new GradeDAO();
+
+		Connection connection = gradeDao.getDAOConnection();
+		connection.setAutoCommit(false);
+		try {
+
+			// Test 3: Wrong type
+			ArrayList<Grade> listGradeResult = gradeDao.getTableGradeByContractTypeId("abcd");
+			ArrayList<Grade> listGradeTest = new ArrayList<>();
+			assertEquals(listGradeTest, listGradeResult);
+
+		} finally {
+			connection.rollback();
+			connection.close();
+		}
+	}
+
+	@Test
+	void testSaveGradeTable1() throws Exception {
+		GradeDAO gradeDao = new GradeDAO();
+
+		Connection connection = gradeDao.getDAOConnection();
+		connection.setAutoCommit(false);
+		try {
+
+			// Test 1: Normal
 			Date date = new Date();
 			java.sql.Timestamp startDate = new java.sql.Timestamp(date.getTime());
 
@@ -79,8 +107,8 @@ class TestGradeDAO extends DAO {
 
 			// If pass => both create and read function work normally
 			assertEquals(listGradeTest, listGradeResult);
-			
-			//Test 2: save Empty Table
+
+			// Test 2: save Empty Table
 			listGradeTest = gradeDao.getTableGradeByContractTypeId("1");
 
 			// Fake data
@@ -89,7 +117,35 @@ class TestGradeDAO extends DAO {
 
 			listGradeResult = gradeDao.getTableGradeByContractTypeId("1");
 
-			// If pass => both create and read function work normally and can handle empty arraylist in input
+			// If pass => both create and read function work normally and can handle empty
+			// arraylist in input
+			assertEquals(listGradeTest, listGradeResult);
+
+		} finally {
+			connection.rollback();
+			connection.close();
+		}
+	}
+
+	@Test
+	void testSaveGradeTable2() throws Exception {
+		GradeDAO gradeDao = new GradeDAO();
+
+		Connection connection = gradeDao.getDAOConnection();
+		connection.setAutoCommit(false);
+		try {
+
+			// Test 2: save Empty Table
+			ArrayList<Grade> listGradeTest = gradeDao.getTableGradeByContractTypeId("1");
+
+			// Fake data
+			ArrayList<Grade> listGradeFakeData = new ArrayList<>();
+			gradeDao.saveGradeTable(listGradeFakeData, 1);
+
+			ArrayList<Grade> listGradeResult = gradeDao.getTableGradeByContractTypeId("1");
+
+			// If pass => both create and read function work normally and can handle empty
+			// arraylist in input
 			assertEquals(listGradeTest, listGradeResult);
 
 		} finally {
